9fc9c17b92f8423e5ed04091931e999ba1a80898
Cat Pien <Catarina.pien@gmail.com>
2021-07-29 16:08:43 -0700

    Edited names of dfs

--- DownloadMultipleFilesInFolderRbind.R
+++ DownloadMultipleFilesInFolderRbind.R
@@ -1,5 +1,6 @@
 ### How to read in all data from a folder and/or bind multiple data frames together ###
 
+# From https://stackoverflow.com/questions/2851327/combine-a-list-of-data-frames-into-one-data-frame-by-row
 # All have the same column header structure
 # Datatest is the name of the folder where the files are located. Might need to add more of the filepath if you are not in a project.
 filenames <- list.files("Datatest", pattern="*.csv", full.names=TRUE)
@@ -10,5 +11,7 @@ ldf <- lapply(filenames, read.csv)
 # bind the dataframes together
 newdf <- bind_rows(ldf, .id = "column_label")
 
+
+###############
 # if you have several different files you want to read in separately, you can rbind them by listing the data frame names below
-bottom <- do.call("rbind", list(ANH_bottom, MAL_bottom, MRZ_bottom, RRI_bottom))
+bottom <- do.call("rbind", list(df1, df2, df3, df4))
